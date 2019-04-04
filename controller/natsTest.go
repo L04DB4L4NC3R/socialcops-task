40aaf9b36316b80887805bdfc4d27799137a243d
angadsharma1016 <angadsharma1016@gmail.com>
2019-04-04 15:19:57 +0530

    spawned a new proc and captured its PID on interrupt signal, SIGKILL syscall

--- controller/natsTest.go
+++ controller/natsTest.go
@@ -3,6 +3,9 @@ package controller
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
 	"time"
 
 	nats "github.com/nats-io/go-nats"
@@ -11,8 +14,10 @@ import (
 var count uint
 
 type Routine struct {
-	ID        uint
-	ShouldRun bool
+	ID          uint
+	ShouldRun   bool
+	IsCompleted bool
+	Err         error
 }
 
 func testNATS(con *nats.EncodedConn) http.HandlerFunc {
@@ -27,26 +32,60 @@ func testNATS(con *nats.EncodedConn) http.HandlerFunc {
 		con.BindRecvQueueChan("foo", "queue", recv)
 
 		// send initiation message
-		sendc <- Routine{count, true}
+		sendc <- Routine{count, true, false, nil}
 
 		// used to cancel task
 		go func() {
 			time.Sleep(time.Second * 5)
-			sendc <- Routine{count, false}
+			sendc <- Routine{count, false, false, nil}
 		}()
 
 		// turn this into a goroutine, task to be cancelled
-		go func(id uint) {
-
+		func(id uint) {
+			var pid int
 			for {
 				msg := <-recv
+
+				if msg.IsCompleted {
+					return
+				}
+
 				if msg.ID == id {
+
+					// run only once
 					if msg.ShouldRun {
 
-						log.Println("Long running task")
+						// spawn long running task
+						cmd := exec.Command("./bin/task")
+						cmd.Stdout = os.Stdout
+						cmd.Stderr = os.Stderr
+
+						err := cmd.Start()
+						if err != nil {
+							log.Fatalf("cmd.Run() failed with %s\n", err)
+						}
+
+						// capture PID
+						pid = cmd.Process.Pid
+						log.Println("Running with pid", pid)
+
+						go func() {
+							err = cmd.Wait()
+							if err != nil {
+								log.Println(err)
+							} else {
+								sendc <- Routine{id, false, true, nil}
+							}
+						}()
+						// ACK for completed JOB sendc <- Routine{id, false}
 
 					} else {
-						log.Println("Exiting")
+						err := syscall.Kill(int(pid), syscall.SIGKILL)
+						if err != nil {
+							log.Println(err)
+						}
+						log.Println(pid)
+						log.Println("Exiting", pid)
 						return
 					}
 				} else {
